0e0564ceb4d260dc49b2da0084d387354b7a2411
Joanne <joannechen84@gmail.com>
2020-09-21 00:31:02 +1000

    added build files for production

---
+++ backend/build/precache-manifest.b242651bf5add991142beeed1d2f1f19.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b991a8cf6475230583864205c0de526",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a5cd60313675ebbf587",
+    "url": "/static/css/2.ed590494.chunk.css"
+  },
+  {
+    "revision": "fcdaa1359a9dfd4237a5",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9a5cd60313675ebbf587",
+    "url": "/static/js/2.48c4c68c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.48c4c68c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcdaa1359a9dfd4237a5",
+    "url": "/static/js/main.cf6d3ba6.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "6cb4e8f957d110360a722235e0f02e3d",
+    "url": "/static/media/StartingBackground.6cb4e8f9.png"
+  },
+  {
+    "revision": "5327aaaa3d114bf3b6ffc31fd0608e6d",
+    "url": "/static/media/adult.5327aaaa.png"
+  },
+  {
+    "revision": "54153fc88c649dddeb10432f3b17da9c",
+    "url": "/static/media/baby.54153fc8.png"
+  },
+  {
+    "revision": "2b73145fa9fd625cc55f35b36948fd3c",
+    "url": "/static/media/egg.2b73145f.png"
+  },
+  {
+    "revision": "6275f614e7a34dc83c5e7bd26c6e5411",
+    "url": "/static/media/teen.6275f614.png"
+  },
+  {
+    "revision": "23e805ae2416b2040c51018efa14a9dc",
+    "url": "/static/media/toddler.23e805ae.png"
+  }
+]);
