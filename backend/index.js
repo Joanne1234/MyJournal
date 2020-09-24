f522ff54ff66fe9c6a1bee24cce833b4c34c4b47
Joanne1234 <44009842+Joanne1234@users.noreply.github.com>
2020-09-25 04:22:19 +1000

    Revert "Trial deploy"

--- backend/index.js
+++ backend/index.js
@@ -38,7 +38,7 @@ app.use("/api/pet", petRoute);
 app.use("/api/misc", miscRoute);
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 
 // Listen to incoming connections
