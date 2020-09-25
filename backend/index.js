2c67121c94b817e99b9df85d9445aa37181a6637
Joanne <joannechen84@gmail.com>
2020-09-25 22:34:55 +1000

    removed body-parser

--- backend/index.js
+++ backend/index.js
@@ -3,21 +3,18 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require('cors')
 const path = require('path');
-const bodyParser = require('body-parser');
 const helmet = require('helmet');
 
 // Functions
 const app = express();
 app.use(helmet())
 app.use(cors())
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-const port = process.env.PORT || 5000
-const CONNECTION_URI = process.env.DB_CONNECTION
-
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+const port = process.env.PORT || 5000
+const CONNECTION_URI = process.env.DB_CONNECTION
+
 
 // Import routes
 const authRoute = require("./routes/auth");
